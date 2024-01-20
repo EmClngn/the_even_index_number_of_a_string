59111ab3190ffcc6f193659cbcafef55a54267ee
unknown <emgcalangian@gmail.com>
2024-01-20 19:14:59 +0800

    pseudo code

---
+++ the_even_index_number_of_a_string.py
@@ -0,0 +1,7 @@
+# Write a program to accept a string from the user and display characters that are present at an even index number.
+
+# Psuedo code
+
+# Ask user input for a string
+
+# Write somethign to separate the even index number of the string and print the characters
