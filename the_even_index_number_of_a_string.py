9317fc54db6025af1c7d602561343723bf4c7e27
unknown <emgcalangian@gmail.com>
2024-01-20 19:17:05 +0800

    user input

--- the_even_index_number_of_a_string.py
+++ the_even_index_number_of_a_string.py
@@ -1,7 +1,9 @@
 # Write a program to accept a string from the user and display characters that are present at an even index number.
 
-# Psuedo code
+# Pseudo code
+
 
 # Ask user input for a string
-
+string = input("What is your desired string?: ")
+print(string)
 # Write somethign to separate the even index number of the string and print the characters
